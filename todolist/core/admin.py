9b288343ed67beb70a72bdfd9e3baa74c40a50c8
letimvkocmoc <letimvkocmoc@gmail.com>
2023-05-18 15:06:08 +1000

    updated

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'first_name', 'last_name', 'image_', 'is_active')
+    readonly_fields = ('image_', 'last_login', 'date_joined')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Изображение', {'fields': ('image_', 'image', )}),
+    )
+    search_fields = ('username', 'email', 'first_name', 'last_name', )
+    filter_horizontal = ()
+    list_filter = ('is_staff', 'is_active', 'is_superuser', )
+    list_per_page = 10
+    list_max_show_all = 100
--- todolist/core/models.py
+++ todolist/core/models.py
@@ -1,6 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+
+    birth_date = models.DateField(null=True, blank=True)
+    image = models.ImageField(
+        _('image'),
+        upload_to='img_users',
+        null=True,
+        blank=True,
+    )
+
+    def image_(self):
+        if self.image:
+            from django.utils.safestring import mark_safe
+            return mark_safe(u'<a href="{0}" target="_blank"><img src="{0}" width="150"/></a>'.format(self.image.url))
+        else:
+            return ('Нет аватарки')
+
+    image._short_description = 'Аватарка'
+    image._allow_tags = True
