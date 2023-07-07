c8b577964743fe6b2364200fbb0716d7fbaf923d
letimvkocmoc <letimvkocmoc@gmail.com>
2023-07-07 22:11:03 +1000

    updated

--- core/views.py
+++ core/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth import get_user_model, login, logout
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
@@ -20,7 +18,6 @@ class LoginView(generics.CreateAPIView):
     serializer_class = LoginSerializer
     permission_classes = [permissions.AllowAny]
 
-    @method_decorator(ensure_csrf_cookie)
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -32,14 +29,11 @@ class LoginView(generics.CreateAPIView):
 class ProfileView(generics.RetrieveUpdateDestroyAPIView):
     model = USER_MODEL
     serializer_class = UserProfileSerializer
-    queryset = USER_MODEL.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
-    @method_decorator(ensure_csrf_cookie)
     def get_object(self):
         return self.request.user
 
-    @method_decorator(ensure_csrf_cookie)
     def delete(self, request, *args, **kwargs):
         logout(request)
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -49,6 +43,5 @@ class UpdatePasswordView(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ChangePasswordSerializer
 
-    @method_decorator(ensure_csrf_cookie)
     def get_object(self):
         return self.request.user
