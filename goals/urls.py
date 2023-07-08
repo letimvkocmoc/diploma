cfd93a9e94c68c8da24505937ed999d777d2d5c3
letimvkocmoc <letimvkocmoc@gmail.com>
2023-07-08 14:59:51 +1000

    updated

--- goals/urls.py
+++ goals/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path("goal_category/list", views.GoalCategoryListView.as_view()),
     path("goal_category/<pk>", views.GoalCategoryView.as_view()),
 
+    path("goal/create", views.GoalCreateView.as_view()),
+    path("goal/list", views.GoalListView.as_view()),
+    path("goal/<pk>", views.GoalDetailView.as_view()),
+
     path("goal_comment/create", views.CommentCreateView.as_view()),
     path("goal_comment/list", views.CommentListView.as_view()),
     path("goal_comment/<pk>", views.CommentDetailView.as_view()),
