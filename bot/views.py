f86da4b05de42c57baa4132a4f4fa2bcf9ad46c7
letimvkocmoc <letimvkocmoc@gmail.com>
2023-07-10 00:31:17 +1000

    updated

--- bot/views.py
+++ bot/views.py
@@ -25,7 +25,6 @@ class VerificationView(GenericAPIView):
         tg_user.save(update_fields=["user"])
         instance_s: TgUserSerializer = self.get_serializer(tg_user)
         tg_client = TgClient(settings.BOT_TOKEN)
-        tg_client.send_message(tg_user.chat_id, "[verification has been completed]")
+        tg_client.send_message(tg_user.chat_id, "Верификация прошла успешно")
 
         return Response(instance_s.data)
-    
